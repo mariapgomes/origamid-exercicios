1a85f900a424fbaa75921dad80c62b454cfe52cd
Maria Gomes <mariapg.dev@gmail.com>
2023-07-13 14:25:30 -0300

    Resolução dos exercícios de tipos de objeto

---
+++ javascript/objetos/tipos-de-objetos/script.js
@@ -0,0 +1,26 @@
+// Liste 5 objetos nativos
+
+Math
+Date
+Set
+JSON
+Map
+
+// Liste 5 objetos do browser
+
+Window
+history
+Document
+HTMLCollection
+NodeList
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+//!Não encontrei até o momento
+
+if(typeof String.fromCharCode !== undefined) {
+  console.log('Existe')
+} else {
+  console.log('Não Existe')
+}
